b3e51fcb653591221620c949d98725317a8fb358
Temitope Enikankiselu <ask4teymitee@gmail.com>
2023-12-13 22:11:02 +0100

    refactor

--- src/Pages/Products/Products/data.js
+++ src/Pages/Products/Products/data.js
@@ -1,4 +1,7 @@
 import lx50 from "../../../images/products/lx50.jpeg"
+import Gx460 from "../../../images/products/WhatsApp Image 2023-12-13 at 19.37.10 (2).jpeg"
+import Gwagon from "../../../images/products/WhatsApp Image 2023-12-13 at 20.04.24 (4).jpeg"
+
 
 
 export const services = [
@@ -6,6 +9,16 @@ export const services = [
       id: 1,
       name: "Lx570 2011 to 2021 super sport",
       img: lx50,
+    },
+    {
+      id: 2,
+      name: "Gx460 lexus 2011 upgrade to 2022 super sport",
+      img: Gx460,
+    },
+    {
+      id: 3,
+      name: "Gwagon Upgrade 2015 to 2021",
+      img: Gwagon,
     },
 
   ];
