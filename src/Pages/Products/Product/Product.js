ceb2c890b527d274a15a9376c94ea08d66da24ba
Temitope Enikankiselu <ask4teymitee@gmail.com>
2023-12-14 12:07:44 +0100

    refactor

--- src/Pages/Products/Product/Product.js
+++ src/Pages/Products/Product/Product.js
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import "./Product.css";
 
 const Product = ({ service }) => {
-  const { _id, name, img, price, description } = service;
+  const { _id, name, imgBefore, imgAfter, price, description } = service;
   const navigate = useNavigate();
 
   const navigateToDetails = (id) => {
@@ -12,7 +12,16 @@ const Product = ({ service }) => {
   return (
     <div className="g-5 gx-5 col-sm-12 col-md-6 col-lg-4 text-center">
       <div className="card mx-auto" style={{ width: "100%" }}>
-        <img src={img} className="card-img-top w-100" alt="..." />
+        {imgBefore ? (
+          <div className="card-img-wrapper">
+            <img src={imgBefore} className="card-img-top w-100" alt="..." />
+            <h4>Before</h4>
+          </div>
+        ) : null}
+        <div className="card-img-wrapper">
+          <img src={imgAfter} className="card-img-top w-100" alt="..." />
+          <h4>After</h4>
+        </div>
         <div className="card-body">
           <h5 className="card-title">{name}</h5>
           <p className="card-text">{description}</p>
--- src/Pages/Products/Products/data.js
+++ src/Pages/Products/Products/data.js
@@ -1,24 +1,32 @@
-import lx50 from "../../../images/products/lx50.jpeg"
-import Gx460 from "../../../images/products/WhatsApp Image 2023-12-13 at 19.37.10 (2).jpeg"
-import Gwagon from "../../../images/products/WhatsApp Image 2023-12-13 at 20.04.24 (4).jpeg"
-
-
+import p1before from "../../../images/products/p1before.jpeg";
+import p1after from "../../../images/products/p1after.jpeg";
+import p2after from "../../../images/products/p2after.jpeg";
+import p3before from "../../../images/products/p3before.jpeg";
+import p3after from "../../../images/products/p3after.jpeg";
+// import p4before from "../../../images/products/p4before.jpeg";
+// import p4after from "../../../images/products/p4after.jpeg";
+// import p5before from "../../../images/products/p5before.jpeg";
+// import p5after from "../../../images/products/p5after.jpeg";
+// import p6before from "../../../images/products/p6before.jpeg";
+// import p6after from "../../../images/products/p6after.jpeg";
 
 export const services = [
-    {
-      id: 1,
-      name: "Lx570 2011 to 2021 super sport",
-      img: lx50,
-    },
-    {
-      id: 2,
-      name: "Gx460 lexus 2011 upgrade to 2022 super sport",
-      img: Gx460,
-    },
-    {
-      id: 3,
-      name: "Gwagon Upgrade 2015 to 2021",
-      img: Gwagon,
-    },
-
-  ];
+  {
+    id: 1,
+    name: "Benz Ml350 2014 to 2019 GLE",
+    imgBefore: p1before,
+    imgAfter: p1after,
+  },
+  {
+    id: 2,
+    name: "Ml350 dashboard upgrade",
+    imgBefore: null,
+    imgAfter: p2after,
+  },
+  {
+    id: 3,
+    name: "Toyota Highlander 2008 upgraded to 2020 vip",
+    imgBefore: p3before,
+    imgAfter: p3after,
+  },
+];
